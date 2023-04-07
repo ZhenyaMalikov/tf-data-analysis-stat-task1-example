b9acc50bded68ca871271dcd169db08b456230c2
ZhenyaMalikov <130054976+ZhenyaMalikov@users.noreply.github.com>
2023-04-07 20:38:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 from scipy.stats import t
 
-chat_id = 516575251 # Ваш chat ID, не меняйте название переменной
+chat_id = 516575251
 
 def solution(x: np.array) -> float:
     n = len(x)
     mean = x.mean()
     std_err = np.std(x, ddof = 1)/np.sqrt(n)
     t_val = t.ppf(0.975, df = n - 1)
-    return (2 * mean)/(t_val * std_err)**2 # Ваш ответ.
+    return (2 * mean)/(t_val * std_err)**2
